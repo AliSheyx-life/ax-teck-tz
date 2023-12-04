9d78e0a6e1bd84709445a8687cb1ed910b218de7
Alisheyx <alisheyx.coder@gmail.com>
2023-12-04 17:43:58 +0500

    feat: added layout component

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { NSpace, NLayoutContent, NLayoutFooter, NLayoutHeader } from "naive-ui";
+</script>
+
+<template>
+  <div class="main-layout">
+    <n-space vertical size="large">
+      <n-layout-header>header</n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+        <slot />
+      </n-layout-content>
+      <n-layout-footer>footer</n-layout-footer>
+    </n-space>
+  </div>
+</template>
